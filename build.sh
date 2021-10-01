614d7f41883d537f4b7d0c0c24f2e78debd04a7d
Brad Jorsch <anomie@users.sourceforge.net>
2021-09-30 23:15:13 -0400

    Update version scraper

--- build.sh
+++ build.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-ZOOM_VER=$(curl -sA 'Mozilla/5.0 (X11; Linux x86_64)' https://zoom.us/download | sed -n 's/.*>Version \([0-9]\+\.[0-9]\+\)\.[0-9]\+ (\([0-9]\+\.[0-9]\+\))<.*/\1.\2/g;T;p;')
+ZOOM_VER=$(curl -sA 'Mozilla/5.0 (X11; Linux x86_64)' https://zoom.us/download | sed -n 's/.*>Version \([0-9]\+\.[0-9]\+\.[0-9]\+\) (\([0-9]\+\))<.*/\1.\2/g;T;p;')
 if [[ -z "$ZOOM_VER" ]]; then
     echo "Failed to fetch Zoom version"
     exit 1
