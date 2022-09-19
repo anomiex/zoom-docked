c47dcfe6f0e1045a547cead132dac336149f30df
Brad Jorsch <anomie@users.sourceforge.net>
2022-09-19 12:04:16 -0400

    Update build script version check
    Instead of screen-scraping their increasingly complicated page, see
    where the download URL redirects to.

--- build.sh
+++ build.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-ZOOM_VER=$(curl -sA 'Mozilla/5.0 (X11; Linux x86_64)' https://zoom.us/download | sed -n 's/.*>Version \([0-9]\+\.[0-9]\+\.[0-9]\+\) (\([0-9]\+\))<.*/\1.\2/g;T;p;')
+ZOOM_VER=$(curl --head https://zoom.us/client/latest/zoom_amd64.deb | sed -nE 's!^location: .*/([0-9]+(\.[0-9]+)+)/zoom_amd64\.deb\r?$!\1!p')
 if [[ -z "$ZOOM_VER" ]]; then
     echo "Failed to fetch Zoom version"
     exit 1
